be625731f02e823a6c9938f351bddc7cd8170e3c
kevin <fsd83811@gmail.com>
2021-01-21 20:41:48 +0800

    draw points

--- src/agv_simulation/agv_simulation/Form1.cs
+++ src/agv_simulation/agv_simulation/Form1.cs
@@ -15,10 +15,11 @@ namespace agv_simulation
         public Form1()
         {
             InitializeComponent();
-            drawOnPic();
+            kevinDrawLine();
+            kevinDrawPoints();
         }
 
-        void drawOnPic()
+        void kevinDrawLine()
         {
             //Console.WriteLine($">> {DateTime.Now.ToString()}");
             pictureBox1.Invalidate();
@@ -32,12 +33,37 @@ namespace agv_simulation
             Pen color = new Pen(Color.Red, 4);
             Brush bb = new SolidBrush(Color.Red);
 
-            Point p1 = new Point(pictureBox1.Width / 2 - 10 , 0);
-            Point p2 = new Point(pictureBox1.Width / 2 - 10 , pictureBox1.Height);
+            Point p1 = new Point(pictureBox1.Width / 2 - 10, 0);
+            Point p2 = new Point(pictureBox1.Width / 2 - 10, pictureBox1.Height);
 
             g.DrawLine(color, p1, p2);
+        }
 
+        void kevinDrawPoints()
+        {
+            //Console.WriteLine($">> {DateTime.Now.ToString()}");
+            pictureBox1.Invalidate();
 
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+
+            Graphics g = Graphics.FromImage(bmp);
+            //Graphics g = pictureBox1.CreateGraphics();
+
+            Pen color = new Pen(Color.Green, 4);
+            Brush bb = new SolidBrush(Color.Red);
+
+            PointF[] points = new PointF[pictureBox1.Height];
+            for (int i = 0; i < pictureBox1.Height; i++)
+            {
+
+                points[i].X = pictureBox1.Width / 2 + (float)Math.Sin(i / (float)pictureBox1.Height * 2 * (Math.PI));
+                //points[i].X = pictureBox1.Width / 2 + (float)Math.Sin((Math.PI)/ (float)180/ (Math.PI) * (float)i *360.0/390.0) * Convert.ToInt32(textBox1.Text) + trackBar1.Value ;
+
+                points[i].Y = (float)i;
+
+            }
+            g.DrawCurve(color, points);
 
         }
 
