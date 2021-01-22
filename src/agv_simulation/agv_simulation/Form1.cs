5e4f713226504758f34a475ed5d8816ecbb536ff
kevin <fsd83811@gmail.com>
2021-01-22 15:04:34 +0800

    closest point

--- src/agv_simulation/agv_simulation/Form1.cs
+++ src/agv_simulation/agv_simulation/Form1.cs
@@ -13,7 +13,11 @@ namespace agv_simulation
     public partial class Form1 : Form
     {
 
-        void genLine()
+        PointF carPoint, closestPoint;
+        PointF[] navPath;
+
+
+        PointF[] genLine()
         {
             int num = 400;
             PointF[] points = new PointF[num];
@@ -24,10 +28,10 @@ namespace agv_simulation
                 points[i].Y = (float)i + 50;
 
             }
-            drawOnPic(points);
+            return points;
         }
 
-        void drawOnPic(PointF[] points)
+        void drawOnPic()
         {
             //Console.WriteLine($">> {DateTime.Now.ToString()}");
             // pictureBox1.Invalidate();
@@ -38,30 +42,61 @@ namespace agv_simulation
             Graphics g = Graphics.FromImage(bmp);
             //Graphics g = pictureBox1.CreateGraphics();
 
-            Pen penColor = new Pen(Color.Red, 4);
-            Brush brushColor = new SolidBrush(Color.Black);
+            Pen penColorRed = new Pen(Color.Red, 4);
+            Brush brushColorBlack = new SolidBrush(Color.Black);
+            Brush brushColorOrange = new SolidBrush(Color.Orange);
 
-            g.DrawCurve(penColor, points);
-            g.FillEllipse(brushColor, pictureBox1.Width / 2 - 100, pictureBox1.Height - 100, 10, 10);
+            g.DrawCurve(penColorRed, navPath);
+            g.FillEllipse(brushColorBlack, carPoint.X - 5, carPoint.Y - 5, 10, 10);
+            g.FillEllipse(brushColorOrange, closestPoint.X - 5, closestPoint.Y - 5, 10, 10);
+
+        }
+
+        PointF findClosestPoint(PointF basic, PointF[] compare)
+        {
+            float errCalc;
+            float err = 999999;
+            PointF point = new PointF(0,0);
+            for(int i = 0; i < compare.Length; i++)
+            {
+                errCalc = Math.Abs(basic.X  - compare[i].X) + Math.Abs(basic.Y  - compare[i].Y);
+                Console.WriteLine("test " + err + " " + errCalc + " " + compare[i].X + " " + compare[i].Y);
+                if(errCalc < err)
+                {
+                    err = errCalc;
+                    point = compare[i];
+                }
+            }
+            return point;
+        }
+
+        void purePursuit()
+        {
 
         }
 
         public Form1()
         {
             InitializeComponent();
-            // genLine();
+
+            navPath = genLine();
+            carPoint = new PointF(pictureBox1.Width / 2 - 100, pictureBox1.Height - 100);
+            closestPoint = findClosestPoint(carPoint, navPath);
+
+            drawOnPic();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             // Console.WriteLine("timer1 tick");
-            // pictureBox1.Invalidate();
+            pictureBox1.Invalidate();
+
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             // Console.WriteLine("pictureBox1_Paint");
-            genLine();
+            drawOnPic();
         }
     }
 }
